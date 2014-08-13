7b3724288cbdbb0736911c8657b24406665d560e
rodrigo ferreira <dilcve@gmail.com>
2014-08-13 00:23:59 -0300

    initial commit

---
+++ Sample Webview/Sample Webview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample Webview
+//
+//  Created by rodrigo ferreira on 13/08/14.
+//  Copyright (c) 2014 Rodrigo Ferreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample Webview/Sample Webview/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample Webview
+//
+//  Created by rodrigo ferreira on 13/08/14.
+//  Copyright (c) 2014 Rodrigo Ferreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
